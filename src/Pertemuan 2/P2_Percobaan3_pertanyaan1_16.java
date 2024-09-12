105f44f4ccc5d58f7b267d34c1963b8e09885940
ibrqp <mibrahimalamin98@gmail.com>
2024-09-12 21:35:02 +0700

    Diubah data KRS menjadi Identitas

--- src/Pertemuan 2/P2_Percobaan3_pertanyaan1_16.java
+++ src/Pertemuan 2/P2_Percobaan3_pertanyaan1_16.java
@@ -4,42 +4,17 @@
 public class P2_Percobaan3_pertanyaan1_16 {
 
         public static void main(String[] args) {
-                System.out.println("                                    KARTU RENCANA STUDI (KRS)\n");
-                System.out.println("NIM             : 244107020062");
-                System.out.println("NAMA            : Muchammad Ibrahim Al Amin");
-                System.out.println("PROGRAM STUDI   : D-IV Teknik Informatika");
-                System.out.println("KELAS           : 1G");
-                System.out.println(
-                                "-------------------------------------------------------------------------------------------");
-                System.out.println(
-                                "|       KODE MK       |                    MATA KULIAH                    |  SKS  |  JAM  |");
-                System.out.println(
-                                "-------------------------------------------------------------------------------------------");
-
-                System.out.println(
-                                "|      RTI241001      | Pancasila                                         |   2   |   2   |");
-                System.out.println(
-                                "|      RTI241002      | Konsep Teknologi Informasi                        |   2   |   4   |");
-                System.out.println(
-                                "|      RTI241003      | Critical Thinking dan Problem Solving             |   2   |   4   |");
-                System.out.println(
-                                "|      RTI241004      | Matematika Dasar                                  |   3   |   6   |");
-                System.out.println(
-                                "|      RTI241005      | Rekayasa Perangkat Lunak                          |   2   |   4   |");
-                System.out.println(
-                                "|      RTI241006      | Dasar Pemrograman                                 |   2   |   4   |");
-                System.out.println(
-                                "|      RTI241007      | Praktikum Dasar Pemrograman                       |   3   |   6   |");
-                System.out.println(
-                                "|      RTI241008      | Keselamatan dan Kesehatan Kerja                   |   2   |   4   |");
-                System.out.println(
-                                "-------------------------------------------------------------------------------------------");
-                System.out.println(
-                                "|      RTI241008      |                       JUMLAH                      |   18  |   34  |");
-                System.out.println(
-                                "-------------------------------------------------------------------------------------------");
-
-                System.out.println("IP Semester Lalu : 0.00");
-                System.out.println("IP Komulatif : 0.00");
+                        System.out.println("---------------------------------------------------------");    
+                        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");    
+                        System.out.println("---------------------------------------------------------");    
+                        System.out.println("Nama    : Muchammad Ibrahim Al Amin");    
+                        System.out.println("NIM     : 244107020062");    
+                        System.out.println("Kelas   : 1G");    
+                        System.out.println("No Absen: 16");    
+                        System.out.println("Prodi   : Teknik Informatika");    
+                        System.out.println("Alamat  : Beji, Kab. Pasuruan");    
+                        System.out.println("No HP   : 085784818488");    
+                        System.out.println("Hobi    : Olahraga");    
+                    
         }
 }
