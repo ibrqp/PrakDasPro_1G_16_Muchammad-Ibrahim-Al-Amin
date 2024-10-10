492c39fb2adda5b775514eddc3fe19348d0cbd74
ibrqp <mibrahimalamin98@gmail.com>
2024-10-10 12:54:30 +0700

    P6_Percobaan1

---
+++ src/pertemuan6/Pemilihan2Percobaan1_16.java
@@ -0,0 +1,23 @@
+package pertemuan6;
+
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan1_16 {
+    public static void main(String[] args) {
+        Scanner input16 = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.print("Masukkan Tahun : ");
+        tahun = input16.nextInt();
+        if((tahun % 4) == 0){
+            if ((tahun % 100) != 0 || (tahun % 400) == 0) 
+                System.out.println("Tahun Kabisat");
+            else
+                System.out.println("Bukan tahun kabisat");
+        } else {
+            System.out.println("Bukan tahun kabisat");
+        }
+
+    }
+}
