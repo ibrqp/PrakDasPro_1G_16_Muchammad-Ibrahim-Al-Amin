1b1bfdf00530280c02d785804b5b5f29e2ee6ba9
Muchammad Ibrahim Al Amin <90238232+ibrqp@users.noreply.github.com>
2024-10-10 13:02:38 +0700

    Update Pemilihan2Percobaan1_16.java

--- src/pertemuan6/Pemilihan2Percobaan1_16.java
+++ src/pertemuan6/Pemilihan2Percobaan1_16.java
@@ -11,13 +11,9 @@ public class Pemilihan2Percobaan1_16 {
         System.out.print("Masukkan Tahun : ");
         tahun = input16.nextInt();
         if((tahun % 4) == 0){
-            if ((tahun % 100) != 0 || (tahun % 400) == 0) 
+            if ((tahun % 100) !=0) 
                 System.out.println("Tahun Kabisat");
-            else
-                System.out.println("Bukan tahun kabisat");
-        } else {
+        }else
             System.out.println("Bukan tahun kabisat");
-        }
-
     }
 }
