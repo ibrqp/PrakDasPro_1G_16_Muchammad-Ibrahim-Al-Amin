1345237cba831c3431e33c869cb8e502e7568370
ibrqp <mibrahimalamin98@gmail.com>
2024-10-10 13:21:50 +0700

    P6_Percobaan1_PertanyaanNo5

--- src/pertemuan6/Pemilihan2Percobaan1_16.java
+++ src/pertemuan6/Pemilihan2Percobaan1_16.java
@@ -8,13 +8,19 @@ public class Pemilihan2Percobaan1_16 {
 
         int tahun;
 
-        System.out.print("Masukkan Tahun : ");
+        System.out.print("Masukkan Tahun: ");
         tahun = input16.nextInt();
-        if((tahun % 4) == 0){
-            if ((tahun % 100) != 0 || (tahun % 400) == 0) 
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) == 0) {
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan tahun kabisat");
+                }
+            } else {
                 System.out.println("Tahun Kabisat");
-            else
-                System.out.println("Bukan tahun kabisat");
+            }
         } else {
             System.out.println("Bukan tahun kabisat");
         }
