3108bd18602eafdf696a552c6edb0e4dc9394184
ibrqp <mibrahimalamin98@gmail.com>
2024-11-01 10:13:31 +0700

    P10_Percobaan1

---
+++ src/Pertemuan10/ArrayBilangan16.java
@@ -0,0 +1,17 @@
+package Pertemuan10;
+
+public class ArrayBilangan16 {
+    public static void main(String[] args) {
+        int [] bil = new int[4];
+
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
+
+        System.err.println(bil[0]);
+        System.err.println(bil[1]);
+        System.err.println(bil[2]);
+        System.err.println(bil[3]);
+    }
+}
