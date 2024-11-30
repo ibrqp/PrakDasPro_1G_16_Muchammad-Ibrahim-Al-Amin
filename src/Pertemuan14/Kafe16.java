0812dc926fe844ae72537e72fc4475767a0bc0a0
ibrqp <mibrahimalamin98@gmail.com>
2024-11-30 08:29:24 +0700

    P13_Percobaan2

--- src/Pertemuan14/Kafe16.java
+++ src/Pertemuan14/Kafe16.java
@@ -1,7 +1,16 @@
 package Pertemuan14;
 
 public class Kafe16 {
-    public static void Menu(){
+
+    
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
+        double diskon = 0.0;
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -11,8 +20,11 @@ public class Kafe16 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("============================");
         System.out.println("Silahkan pilih menu yang Anda inginkan");
+
     }
+
     public static void main(String[] args) {
-        Menu();
+        Menu("Andi", true, "DISKON50");
     }
 }
+
