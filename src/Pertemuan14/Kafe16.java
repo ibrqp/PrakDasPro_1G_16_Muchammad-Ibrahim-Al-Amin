c538b2486586a660c4866dc7b527bbc0f643ab51
ibrqp <mibrahimalamin98@gmail.com>
2024-11-30 08:30:19 +0700

    P13_Percobaan3

--- src/Pertemuan14/Kafe16.java
+++ src/Pertemuan14/Kafe16.java
@@ -1,5 +1,7 @@
 package Pertemuan14;
 
+import java.util.Scanner;
+
 public class Kafe16 {
 
     
@@ -21,10 +23,38 @@ public class Kafe16 {
         System.out.println("============================");
         System.out.println("Silahkan pilih menu yang Anda inginkan");
 
+        if (kodePromo == "DISKON50") {
+            diskon = 0.5;
+            System.out.println("\nAnda mendapatkan diskon 50%");
+        } else if (kodePromo == "DISKON30") {
+            diskon = 0.3;
+            System.out.println("\nAnda mendapatkan diskon 30%");
+
+        } else {
+            System.out.println("Kode Invalid");
+        }
+    }
+
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+            int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+          
+            return hargaTotal;
+
     }
 
     public static void main(String[] args) {
         Menu("Andi", true, "DISKON50");
+
+        Scanner sc = new Scanner(System.in);
+            System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+            int pilihanMenu = sc.nextInt();
+            System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+            int banyakItem = sc.nextInt();
+
+            int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+
+            System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga);
     }
 }
-
