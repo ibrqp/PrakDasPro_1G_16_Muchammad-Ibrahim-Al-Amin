a9a13a35edb534d8e1b2912e0c817f2be84a6c69
ibrqp <mibrahimalamin98@gmail.com>
2025-02-27 08:52:08 +0700

    P3_Percobaan2_PertanyaanNo1

--- src/semester2/Praktikum03/Mahasiswa15.java
+++ src/semester2/Praktikum03/Mahasiswa15.java
@@ -1,8 +1,18 @@
 package semester2.Praktikum03;
 
 public class Mahasiswa15 {
+
     public String nim;
     public String nama;
     public String kelas;
     public float ipk;
-}
+
+    void cetakInfo(){
+        // System.out.println("Data Mahasiswa ke-" + 1));
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("ipk   : " + ipk);
+        System.out.println("-----------------------------");
+    }
+} 
