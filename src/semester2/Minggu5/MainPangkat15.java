fa44e772ab3efcb2ff37a999105f4888576d4507
ibrqp <mibrahimalamin98@gmail.com>
2025-03-13 10:11:29 +0700

    P5_Percobaan2_PertanyaanNo3

--- src/semester2/Minggu5/MainPangkat15.java
+++ src/semester2/Minggu5/MainPangkat15.java
@@ -19,7 +19,7 @@ public class MainPangkat15 {
         }
         System.out.println("HASIL PANGKAT BRUTEFORCE:");
         for (Pangkat15 p : png) {
-            System.out.println(p.nilai + "^" + p.pangkat + ": " + p.pangkatBF(p.nilai, p.pangkat));
+            System.out.println(p.nilai + "^" + p.pangkat + ": " + p.pangkatBF());
         }
 
         System.out.println("HASIL PANGKAT DIVIDE AND CONQUER:");
--- src/semester2/Minggu5/Pangkat15.java
+++ src/semester2/Minggu5/Pangkat15.java
@@ -8,13 +8,14 @@ public class Pangkat15 {
         pangkat = p;
     }
 
-    int pangkatBF(int a, int n) {
+    int pangkatBF() {
         int hasil = 1;
-        for (int i = 0; i < n; i++) {
-            hasil = hasil * a;
+        for (int i = 0; i < pangkat; i++) { // Menggunakan atribut pangkat
+            hasil *= nilai; // Menggunakan atribut nilai
         }
         return hasil;
     }
+    
  
     int pangkatDC(int a, int n) {
         if (n == 1) {
