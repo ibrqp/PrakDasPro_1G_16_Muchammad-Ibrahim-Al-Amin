09e4160e58699a1fd0760aca8fe7797422dbab72
ibrqp <mibrahimalamin98@gmail.com>
2025-03-27 13:28:10 +0700

    P7_Percobaan2_PertanyaanNo4

--- src/semester2/Praktikum06/MahasiswaBerprestasi15.java
+++ src/semester2/Praktikum06/MahasiswaBerprestasi15.java
@@ -12,12 +12,14 @@ public class MahasiswaBerprestasi15 {
             System.out.println("data sudah penuh");
         }
     }
+
     void tampil() {
         for (Mahasiswa15 m : listMhs) {
             m.tampilkanInformasi();
             System.out.println("------------------------------------");
         }
     }
+
     int sequentialSearching(double cari) {
         int posisi = -1;
         for (int j = 0; j < listMhs.length; j++) {
@@ -37,14 +39,29 @@ public class MahasiswaBerprestasi15 {
         }
     }
 
-    void tampilDataSearch(double x, int pos){
-        if(pos != 1){
-            System.out.println("nim\t : "+listMhs[pos].nim);
-            System.out.println("nama\t : "+listMhs[pos].nama);
-            System.out.println("kelas\t : "+listMhs[pos].kelas);
-            System.out.println("ipk\t : "+ x);
-        }else{
-            System.out.println("Data mahasiswa dengan IPK "+ x + " tidak ditemukan");
+    void tampilDataSearch(double x, int pos) {
+        if (pos != 1) {
+            System.out.println("nim\t : " + listMhs[pos].nim);
+            System.out.println("nama\t : " + listMhs[pos].nama);
+            System.out.println("kelas\t : " + listMhs[pos].kelas);
+            System.out.println("ipk\t : " + x);
+        } else {
+            System.out.println("Data mahasiswa dengan IPK " + x + " tidak ditemukan");
         }
+    }
+
+    int findBinarySearch(double cari, int left, int right) {
+        int mid;
+        if (right >= left) {
+            mid = (left + right) / 2;
+            if (cari == listMhs[mid].ipk) {
+                return (mid);
+            } else if (listMhs[mid].ipk < cari) {
+                return findBinarySearch(cari, left, mid - 1);
+            } else {
+                return findBinarySearch(cari, mid + 1, right);
+            }
+        }
+        return -1;
     }
 }
